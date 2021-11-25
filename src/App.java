00a8ef5e2f6380817011c90e2486fc09ccda2b3b
MrAlhadi <m.m.h.alhadi@gmail.com>
2021-11-25 19:24:23 +0200

    FistPrintOut

---
+++ src/App.java
@@ -0,0 +1,15 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("\\Hello, World!\n");
+        System.out.println("\"Hello, World!\"");
+        System.out.println();
+        
+
+        // this is comment
+        /*
+        *ef
+        *
+        */
+
+    }
+}
