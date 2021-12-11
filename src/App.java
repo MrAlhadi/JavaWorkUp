8127beacefed3acb97072c8b8bc24882bd3b6d96
MrAlhadi <m.m.h.alhadi@gmail.com>
2021-12-11 22:46:25 +0200

    user inpu Scanner

--- src/App.java
+++ src/App.java
@@ -1,14 +1,17 @@
+import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
-         String x= "AA";
-         String y= "BB";
-         String temp;
-         temp=x;
-         x=y;
-         y=temp;
-         System.out.println("X is:"+x);
-         System.out.println("Y is:"+y);
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("What is your name?");
+        String name= scanner.nextLine();
+        System.out.println("How old are you? ");
+        int age=scanner.nextInt();
+        System.out.println("What is your best food? ");
+        scanner.nextLine();
+        String food=scanner.nextLine();
+        System.out.println("Hello "+ name+" . ");
+        System.out.println("You are "+age+" years old . ");
+        System.out.println("Your best food is "+food+"  . ");
 
     }
 }
